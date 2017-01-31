47b29ec4eac5e0835378d3bd3e0848f854b1be72
Hiroyuki Kano <wukann@gmail.com>
2017-02-01 00:57:03 +0900

    change packaging script

---
+++ script/Clean-Temp.ps1
@@ -0,0 +1,1 @@
+Remove-Item ./tmp -Force -Recurse
--- script/Package-Modules.ps1
+++ script/Package-Modules.ps1
@@ -1,7 +1,6 @@
 New-Item ./tmp -ItemType Directory -Force
 New-Item ./dist -ItemType Directory -Force
-Copy-Item -Path ./src/index.js -Destination ./tmp
+Copy-Item -Path ./index.js -Destination ./tmp
 Copy-Item -Path ./node_modules/moment -Destination ./tmp/node_modules/moment -Recurse
 Copy-Item -Path ./node_modules/moment-timezone -Destination ./tmp/node_modules/moment-timezone -Recurse
-Compress-Archive -Path ./tmp/* -DestinationPath ./dist/package.zip -Force
-Remove-Item ./tmp -Force -Recurse
+
